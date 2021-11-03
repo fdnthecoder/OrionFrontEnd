866bef9c33d39db44daac161ec4b2c58f56da092
meij03 <jm7787@nyu.edu>
2021-11-03 15:44:53 -0400

    updating frontend repo with codes

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Login from './Pages/Login/Login';
+import Profile from './Pages/Profile/Profile';
+//import React, { useEffect, useState } from 'react';
+//import axios from 'axios'
+
+class App extends Component{
+	render(){
+		return (
+			<div className="App">
+				<Header />
+	
+				<Switch>
+					<Route path='/login' component={Login}/>
+					<Route path='/profile' component={Profile}/>
+					<Route exact path='/' render={() => <h1>Hello Gang!</h1>}/>
+				</Switch>
+			</div>
+		);
+	}
+}
+
+//<Route path='/login' render={() =><h1>Hello Gang</h1>}/>
+export default App;
