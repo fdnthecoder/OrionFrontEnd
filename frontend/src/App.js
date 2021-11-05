c7bf9776b59d588d0b9518244b1d85eb0e8316a9
meij03 <jm7787@nyu.edu>
2021-11-05 16:17:11 -0400

    Testing backend connection to fontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
 	const [state, setState] = useState({})
 
 	useEffect(() => {
-		axios.get("https://orion-crepe.herokuapp.com/frontend/index").then(response => {
+		axios.get("https://orion-crepe.herokuapp.com/hello").then(response => {
 			if (response.status == 200){
 				return response.json()
 			}
