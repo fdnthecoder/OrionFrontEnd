4b6eeff2b32aaf1ed1435f6de2a104e48284967d
meij03 <jm7787@nyu.edu>
2021-11-05 13:47:46 -0400

    Testing frontend intitizling

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+    import './Header.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import logoURL from '../../Assets/logo.png'
+
+const Header = () => {
+    return(
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img src={logoURL} alt="logo" width="30" height="30" className="d-inline-block align-top" />{' '}
+                    Orion
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Nav className="me-auto">
+                    <Nav.Link href="">Companies</Nav.Link>
+                    <Nav.Link href="">Internships</Nav.Link>
+                </Nav >
+                <Nav>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/profile">Profile</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header
---
+++ makefile
@@ -0,0 +1,6 @@
+heroku-run:
+	heroku login
+	git init
+	heroku git:remote -a orion-crepe-frontend
+	heroku config:set PYTHONPATH="/app"
+	heroku config:set ORION_HOME="/app"
