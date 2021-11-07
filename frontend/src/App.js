ce633543207170fefb1479051047c6cedd3444ab
meij03 <jm7787@nyu.edu>
2021-11-07 10:18:29 -0500

    Fixing test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
 	const [state, setState] = useState({})
 
 	useEffect(() => {
-		axios.get("https://orion-crepe.herokuapp.com/hello").then(response => {
+		axios.get("https://orion-crepe.herokuapp.com/").then(response => {
 			if (response.status == 200){
 				return response.json()
 			}
