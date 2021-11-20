c5c8c8ce3e6bf220b2795c0ef5b68e16fbce99cb
meij03 <jm7787@nyu.edu>
2021-11-20 14:59:02 -0500

    Working on post request register page

--- frontend/src/Pages/Register/Register.js
+++ frontend/src/Pages/Register/Register.js
@@ -9,49 +9,64 @@ class Register extends React.Component{
     constructor (props){
         super(props);
         this.state = {
-            data: "", 
-            create: true,
+            username: "",
+            password: "",
         };
-        this.click = this.click.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this)
+        this.onClick = this.onClick.bind(this);
     }
 
-    click() {
-        this.setState({ create: true})
-        axios.post("https://orion-crepe.herokuapp.com/user", {}).then((response) => {
-            console.log(response.data);
-            this.setState({ 
-                data:response.data,
-                create: true}) 
-            })
-            .catch((err) => {
-                this.setState({username:err, create: false,})
-                console.log(err);
-            });
+    onChangeUsername(event){
+        this.setState({ username: event.target.username });
+        console.log(event.target.username)
     }
+
+    onChangePassword(event){
+        this.setState({ password: event.target.password});
+        console.log(event.target.password)
+    }
+    onClick(event) {
+        alert('A name was submitted: ' + this.state.username);
+        alert("a password has been submitted: ", this.state.password)
+        event.preventDefault();
+        //https://orion-crepe.herokuapp.com
+        axios.post("http://127.0.0.1:8000/user", {
+            username: this.state.username,
+            password: this.state.password,
+        }).then((response) => {
+            // console.log(this.state.username);
+            // console.log(this.state.password);
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
     render(){
         return(
             <div className="register-body">
                 <div className="register-div">
                     <h1>Register a new account</h1>
                     <div>
-                        <Form>
+                        <Form onSubmit = {this.onClick}>
                             <Form.Group>
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control type="username" placeholder="Username" />
+                                <Form.Control type="username" value={this.state.username} onChange={event => this.onChangeUsername(event)}  placeholder="Username" />
                             </Form.Group>   
                             <Form.Group>
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control type="password" value= {this.state.password} onChange={event => this.onChangePassword(event)} placeholder="Password" />
                             </Form.Group>
                             <Form.Group>
                                 <Form.Label>Confirm your password</Form.Label>
                                 <Form.Control type="password" placeholder="Confirm Password" />
                             </Form.Group>
                             <br />   
-                            <Button onClick={this.click} variant="outline-dark" type="submit">
+                            <Button variant="outline-dark" type="submit">
                                 Register
                             </Button>{' '}
-                            {this.state.data}
                         </Form>
                     </div>
                 </div>
