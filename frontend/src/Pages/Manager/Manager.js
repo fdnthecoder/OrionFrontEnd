91f465eccbb21a86c569192fa243c73df6c741ed
itsweiwei <wz1340@nyu.edu>
2021-11-23 12:47:05 -0500

    Fixing internship

--- frontend/src/Pages/Manager/Manager.js
+++ frontend/src/Pages/Manager/Manager.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 class Manager extends React.Component{
     render() {
         return(
-            <Container>
-                
-            </Container>
+            <DndProvider backend={HTML5BBackend}>
+                <Container>
+                    <div>In progress</div>
+                </Container>
+                <Container>
+                    <div>Interviewing for</div>
+                </Container>
+                <Container>
+                    <div>Complete</div>
+                </Container>
+            </DndProvider>
         )
     }
 }
