5638d7cbcdee00d846b8d5e25a2688e200d5c6ff
itsweiwei <wz1340@nyu.edu>
2021-11-23 12:57:52 -0500

    removed error where Internships page would not load correctly

--- frontend/src/Pages/Internships/Internships.js
+++ frontend/src/Pages/Internships/Internships.js
@@ -8,19 +8,7 @@ class Internships extends React.Component{
     - receiving a list of data from backend
     - displaying list on our board (will need to pass as a prop) 
     */
-
-    constructor (props){
-        super(props);
-        this.state = {
-            email: "", 
-            password: ""
-        };
-        this.changeEmail = this.changeEmail.bind(this);
-        this.changePassword = this.changePassword.bind(this);
-        this.onClick = this.onClick.bind(this);
-    }
-
-    
+   
     render(){
         return(
             <Container>
