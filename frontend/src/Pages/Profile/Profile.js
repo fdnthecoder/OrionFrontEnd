aa61a7ea3fe7461ff269a20f386ea5951204d5fa
itsweiwei <wz1340@nyu.edu>
2021-11-27 14:02:52 -0500

    fixed error where invisble HEAD is in Profile.js

--- frontend/src/Pages/Profile/Profile.js
+++ frontend/src/Pages/Profile/Profile.js
@@ -1,63 +1,46 @@
-import React from 'react'
-import './Profile.css'
-import Image from 'react-bootstrap/Image'
-import Container from 'react-bootstrap/Container'
-import logoURL from '../../Assets/defaultProfilePic.png'
-import axios from 'axios'
+import React from "react"
+import { Button, Card, Container, Row, Col} from "react-bootstrap"
 
-class Profile extends React.Component{
-<<<<<<< HEAD
+class Board extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            user: {
-                name: "Jane Doe",
-
-            }
-        }
-    }
-
-=======
-    constructor(props) {
-        super(props);
-        this.state = {
-            name:"",
-            username:"diallodb",
-            email:"",
+            listings: [
+                {
+                    id: 0,
+                    jobName : "Software Engineer @ AWS",
+                    jobDesc : "Some stuff that software engineers at Amazon Web Services would do I guess"
+                },
+                {
+                    id: 1,
+                    jobName : "Data Engineer @ AWS",
+                    jobDesc : "Seriously, like what even do these people do lmao"
+                }
+            ]
         };
-        this.getProfile = this.getProfile.bind(this);
-        
     }
 
-    getProfile(event){
-        axios.get( "http://127.0.0.1:8000/profile", {
-            username: this.state.username,
-        }).then((response) => {
-            this.setState({name:response.data.name, email:response.data.email});
-            console.log(response)
-        }).catch((err) => {
-            console.log(err);
-        })
-    };
-
->>>>>>> 86639c31e0b7911efe7956f6c8532b7498a71254
     render(){
         return(
             <Container fluid>
-                {this.getProfile}
-                <br />
-<<<<<<< HEAD
-                <Image align-item="center" src={logoURL} roundedCircle/>
-                <h1 align="center">{this.state.user.name}</h1>
-=======
-                <Image src={logoURL} roundedCircle/>
-                <h1>{this.state.name}</h1>
-                <h2>{this.state.username}</h2>
-                <h2>{this.state.email}</h2>
->>>>>>> 86639c31e0b7911efe7956f6c8532b7498a71254
+                <Row xs={1} md={2} className="g-4">
+                {this.state.listings.map(listings => (
+                    <Col key={"col" + listings.jobName}>
+                        <Card key={"entry" + listings.jobName} >
+                            <Card.Body key={"body" + listings.jobName}>
+                            <Card.Title key={"title" + listings.jobName}>{listings.jobName}</Card.Title>
+                            <Card.Text key={"text" + listings.jobName} >
+                               {listings.jobDesc}
+                            </Card.Text>
+                            <Button variant="outline-success"> View </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+                </Row>
             </Container>
-        );
+        )
     }
 }
 
-export default Profile;
+export default Board;
