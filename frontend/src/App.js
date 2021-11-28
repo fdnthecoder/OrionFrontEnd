b86c812bd146b96fed6f5d7acaa11534a8da52e4
meij03 <jm7787@nyu.edu>
2021-11-28 02:29:06 -0500

    Profile page is up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,28 +7,13 @@ import Post from './Pages/Post/Post';
 import Internships from "./Pages/Internships/Internships";
 import PostInternships from './Pages/PostInternship/PostInternship';
 import Register from "./Pages/Register/Register";
-import {Test} from "./Components/Test/Test";
 import Home from './Pages/Home/Home';
-import { useEffect, useState } from 'react';
-import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-	const [state, setState] = useState({})
-
-	useEffect(() => {
-		axios.get(process.env.REACT_APP_BASEURL+"/hello").then(response => {
-			if (response.status){
-				console.log(response.data);
-				return response.data
-			}
-		}).then(data => setState(data))
-		.then(error => console.log(error))
-	},[])
 
 	return (
 		<div className="App">
-			<Test prop={state}/>
 			<Header />
 			<Switch>
 				<Route path='/internships/new' component={PostInternships} />
--- frontend/src/Pages/Profile/Profile.js
+++ frontend/src/Pages/Profile/Profile.js
@@ -2,38 +2,36 @@ import React from 'react'
 import './Profile.css'
 import Image from 'react-bootstrap/Image'
 import logoURL from '../../Assets/defaultProfilePic.png'
-import { Container} from "react-bootstrap"
+import {  Card, Container, Row, Col} from "react-bootstrap"
 import axios from 'axios';
 
 class Profile extends React.Component{
-
     constructor(props){
         super(props);
         this.state = {
             username: "qadriid",
             email: "",
             applications:[],
-            data:{},
-            isMount: false, 
+            data:[],
+            isMount: "false",
         }
     }
 
+
 	componentDidMount(){
-        this.setState({isMount:true})
-		axios.get( "https://orion-crepe.herokuapp.com/profile",
+        this.isMount = true;
+		axios.get(process.env.REACT_APP_BASEURL + "profile",
         {
             params:{
                 username: this.state.username,
             }
         }).then(response => {
-            console.log(response.data);
+
             if (this.isMount){
-                this.setState({
-                    data: response.data,
-                    email: this.data.email,
-                    applications: this.data.username,
-                })
-                console.log(response.data);
+                this.setState({data: response.data});
+                this.setState({email: response.data.email});
+                this.setState({applications: response.data.applications})
+
             }
 		}).catch((err) => {
             console.log(err);
@@ -48,21 +46,23 @@ class Profile extends React.Component{
                 <Image src={logoURL} roundedCircle/>
                 <h1>Hello {this.state.username}!</h1>
                 <h2>Email: {this.state.email}</h2>
-                <p> applications : {this.state.applications}</p>
-                {/* <Row xs={1} md={2} className="g-4">
-                {this.applications.map(listings => (
-                    <Col key={"col" + listings.jobName}>
-                        <Card key={"entry" + listings.jobName} >
-                            <Card.Body key={"body" + listings.jobName}>
-                            <Card.Title key={"title" + listings.jobName}>{listings.jobName}</Card.Title>
-                            <Card.Text key={"text" + listings.jobName} >
-                                {listings.jobDesc}
-                            </Card.Text>
+                <Row xs={1} md={2} className="g-4">
+                {this.state.applications.map(listings => (
+                    <Col key={"col" + listings.postID}>
+                        <Card key={"entry" + listings.postID} >
+                            <Card.Body key={"body" + listings.status}>
+                            <Card.Title key= {"status" + listings.status}> {listings.status} </Card.Title>
+                            <Card.Title key={"company" + listings.company}>{listings.company}</Card.Title>
+                            <Card.Title key={"title" + listings.title}>{listings.title}</Card.Title>
+
+                            <Card.Link href= {listings.url} key={"url" + listings.url} >
+                                {listings.url}
+                            </Card.Link>
                             </Card.Body>
                         </Card>
                     </Col>
                 ))}
-                </Row> */}
+                </Row>
             </Container>
         );
     }
