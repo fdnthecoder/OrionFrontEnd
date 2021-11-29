b78898b6bb125375650b3f5db618c14894cd9a06
itsweiwei <wz1340@nyu.edu>
2021-11-29 01:21:05 -0500

    layout for board appearing on Profile but I'm dead from work

--- frontend/src/Pages/Manager/Manager.js
+++ frontend/src/Pages/Manager/Manager.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
+import {Row, Col, Card, ListGroup} from 'react-bootstrap';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
+const hardcodedApps = [
+    {
+        "postId": 0,
+        "status": "applied",
+        "company": "google",
+        "title": "Software Engineering Intern, Bachelor's, Summer 2022",
+        "url": "https://careers.google.com/jobs/results/115615177363071686-software-engineering-intern-bachelors-summer-2022/?utm_campaign=google_jobs_apply&utm_medium=organic&utm_source=google_jobs_apply"
+    },
+    {
+        "postId": 1,
+        "status": "saved",
+        "company": "google",
+        "title": "Software Engineering Intern PhD Summer 2022",
+        "url": "https://www.jobilize.com/amp/job/software-engineering-intern-phd-summer-2022-madison-google-united?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic"
+    }
+];
+
 const Manager = (props) => {
-    const [applications, updateApplications] = useState(props.applications)
+    const [applications, updateApplications] = useState(hardcodedApps)
     function handleOnDragEnd(result) {
         if (!result.destination) return;
     
@@ -14,27 +32,55 @@ const Manager = (props) => {
       }
     return(
         <DragDropContext onDropEnd={handleOnDragEnd}>
+            <Row xs={1} md={2} className="g-4">
+            <Col>
+            <h2>Interested</h2>
             <Droppable droppableId="interested">
                 {(provided) => (
-                    <ul className="interested" {...provided.droppableProps} ref={provided.innerRef}>
+                    <ListGroup className="interested" {...provided.droppableProps} ref={provided.innerRef}>
                     { applications.map(applications => {
                             return (
-                              <Draggable key={applications.jobName} draggableId={applications.jobName} index={applications.jobDesc}>
+                              <Draggable key={"draggable"+ applications.postId} draggableId={"interested-job"+ applications.postId} index={applications.postId}>
                                 {(provided) => (
-                                  <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                    <p>
-                                      owo
-                                    </p>
-                                  </li>
+                                  <Card key={"job"+ applications.postId}ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                    <Card.Title key={"title"+applications.postId}>{applications.title}</Card.Title>
+                                    <Card.Body key={"body"+applications.postId}>{applications.url}</Card.Body>
+                                    <Card.Footer key={"status"+applications.postId}>{applications.status}</Card.Footer>
+                                  </Card>
                                 )}
                               </Draggable>
                             );
                           })}
                           {provided.placeholder}
-                        </ul>
+                        </ListGroup>
                       )}    
-                    </Droppable>
-                  </DragDropContext>
+            </Droppable>
+            </Col>
+            <Col>
+            <h2>Applied</h2>
+            <Droppable droppableId="applied">
+                    {(provided) => (
+                        <ListGroup className="applied" {...provided.droppableProps} ref={provided.innerRef}>
+                        { applications.map(applications => {
+                                return (
+                                <Draggable key={"draggable"+ applications.postId} draggableId={"applied-job"+ applications.postId} index={applications.postId}>
+                                    {(provided) => (
+                                    <Card key={"job"+ applications.postId}ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                        <Card.Title key={"title"+applications.postId}>{applications.title}</Card.Title>
+                                        <Card.Body key={"body"+applications.postId}>{applications.url}</Card.Body>
+                                        <Card.Footer key={"status"+applications.postId}>{applications.status}</Card.Footer>
+                                    </Card>
+                                    )}
+                                </Draggable>
+                                );
+                            })}
+                            {provided.placeholder}
+                            </ListGroup>
+                        )}    
+            </Droppable>
+            </Col>
+            </Row>
+        </DragDropContext>
     );
 }
 
--- frontend/src/Pages/Profile/Profile.js
+++ frontend/src/Pages/Profile/Profile.js
@@ -18,7 +18,6 @@ class Profile extends React.Component{
         }
     }
 
-
 	componentDidMount(){
         this.isMount = true;
 		axios.get('https://orion-crepe.herokuapp.com/' + "profile",
@@ -47,23 +46,24 @@ class Profile extends React.Component{
                 <h1>Hello {this.state.username}!</h1>
                 <h2>Email: {this.state.email}</h2>
                 <br />
+                <h2>All jobs you've interacted with</h2><br />
                 <Row xs={1} md={2} className="g-4">
                 {this.state.applications.map(listings => (
                     <Col key={"col" + listings.postID}>
                         <Card key={"entry" + listings.postID} >
-                            <Card.Body key={"body" + listings.status}>
-                            <Card.Title key= {"status" + listings.status}> {listings.status} </Card.Title>
-                            <Card.Title key={"company" + listings.company}>{listings.company}</Card.Title>
                             <Card.Title key={"title" + listings.title}>{listings.title}</Card.Title>
-
-                            <Card.Link href= {listings.url} key={"url" + listings.url} >
-                                {listings.url}
-                            </Card.Link>
+                            <Card.Title key={"company" + listings.company}>{listings.company}</Card.Title>
+                            <Card.Body key={"body" + listings.status}>
+                                <Card.Link href= {listings.url} key={"url" + listings.url} >
+                                    Check out the official listing here
+                                </Card.Link>
                             </Card.Body>
+                            <Card.Footer key={"footer"+ listings.status}>Status: {listings.status}</Card.Footer>
                         </Card>
                     </Col>
                 ))}
-                </Row>
+                </Row><br />
+                <h2>Manage your job applications below!</h2><br />
                 <Manager applications={this.state.applications} />
             </Container>
         );
