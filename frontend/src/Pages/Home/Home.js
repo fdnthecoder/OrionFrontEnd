e581c5112846d8d98cb73fa38eebea6c5832256e
itsweiwei <wz1340@nyu.edu>
2021-11-30 13:22:49 -0500

    added more content to Home to remove annoying whitespace:

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import  { Image, Carousel, Container } from 'react-bootstrap'
 import bannerURL from "../../Assets/banner.jpg"
 import backgroundURL from "../../Assets/background.jpg"
+import aboutURL from "../../Assets/aboutImage.jpg"
 
 class Home extends React.Component{
     render(){
@@ -23,6 +24,15 @@ class Home extends React.Component{
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
+                <Container className="me-auto" style={{padding: "20px", display: "flex", justifyContent: "center"}}>
+                    <Container>
+                        <h1 style={{padding: "20px", textAlign: "center"}}>Who we are</h1>
+                        <p style={{fontSize: "14pt"}}>
+                           Orion is a product started up by students in search of a way to streamline the application process for internships and jobs. The intention is to organize data that job hunters would normally have trouble organizing through the use of spreadsheets in a more simpleer and aesthetically pleasing through the use of React and React-Bootstrap.   
+                        </p>
+                    </Container>
+                    <Image style={{maxWidth: "40rem", paddingLeft: "20px"}} src={aboutURL} alt="aboutImage" />
+                </Container>
             </Container>
         );
     }
